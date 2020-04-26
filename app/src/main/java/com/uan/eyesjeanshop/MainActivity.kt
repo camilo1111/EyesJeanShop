bcd2f2284b2583524b9d03093ab50ab3888006fa
CAMILO <cdiaz29@uan.edu.co>
2020-04-25 19:14:04 -0500

    add activity list products

--- app/src/main/java/com/uan/eyesjeanshop/MainActivity.kt
+++ app/src/main/java/com/uan/eyesjeanshop/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.uan.eyesjeanshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageButtonSearch.setOnClickListener{
+            val continueButton = Intent (this, ListProductActivity::class.java)
+            startActivity(continueButton)
+        }
+
+
     }
 }
