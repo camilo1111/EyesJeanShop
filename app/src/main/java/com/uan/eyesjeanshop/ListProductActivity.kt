a2c1f163bf38b06058d4d056e3672b15638ad4bb
CAMILO <cdiaz29@uan.edu.co>
2020-04-26 12:05:03 -0500

    add activity list products

--- app/src/main/java/com/uan/eyesjeanshop/ListProductActivity.kt
+++ app/src/main/java/com/uan/eyesjeanshop/ListProductActivity.kt
@@ -1,13 +1,42 @@
 package com.uan.eyesjeanshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_list_product.*
 
 class ListProductActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_product)
+
+        ListViewProducts.setOnItemClickListener { _, item, _, _ ->
+            val text = item as TextView
+
+            when(text.text.toString()){
+
+                "Producto 1"-> {val started = Intent(this, DetailProductActivity::class.java)
+                    startActivity(started)
+                    finish()}
+                "Producto 2"-> {val started = Intent(this, DetailProductActivity::class.java)
+                    startActivity(started)
+                    finish()}
+                "Producto 3"-> {val started = Intent(this, DetailProductActivity::class.java)
+                    startActivity(started)
+                    finish()}
+                "Producto 4"-> {val started = Intent(this, DetailProductActivity::class.java)
+                    startActivity(started)
+                    finish()}
+                "Producto 5"-> {val started = Intent(this, DetailProductActivity::class.java)
+                    startActivity(started)
+                    finish()}
+                "Producto 6"-> {val started = Intent(this, DetailProductActivity::class.java)
+                    startActivity(started)
+                    finish()}
+            }
+        }
 
 
     }
